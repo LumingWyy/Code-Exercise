f45c6ea9ab9b0e639ef128dc8c8627ef04b49d94
Wang Luming <31833334+LumingWyy@users.noreply.github.com>
2018-03-08 11:44:47 +0900

    Update spider1.py

--- spider1.py
+++ spider1.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 '''
 Created on 2018/03/06
 
@@ -15,21 +15,4 @@ request=urllib2.Request(url,data)   #post
 #request = urllib2.Request("%s?%s"%(url,data))   #get
 response=urllib2.urlopen(request)
 print response.read()
-=======
-'''
-Created on 2018/03/06
 
-@author: WangLuming
-'''
-import urllib
-import urllib2
-values={}
-values['username']=""
-values['password']=""
-data=urllib.urlencode(values)
-url=""
-request=urllib2.Request(url,data)   #post
-#request = urllib2.Request("%s?%s"%(url,data))   #get
-response=urllib2.urlopen(request)
-print response.read()
->>>>>>> 14932c652195090c7430093435fcbd365d3d5ae3
