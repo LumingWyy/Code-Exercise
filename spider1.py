ed3defeb0462c2a87237d063245c1b92d8d03948
LumingWyy <LumingWyy@users.noreply.github.com>
2018-03-08 11:42:02 +0900

    add

---
+++ spider1.py
@@ -0,0 +1,16 @@
+'''
+Created on 2018/03/06
+
+@author: WangLuming
+'''
+import urllib
+import urllib2
+values={}
+values['username']=""
+values['password']=""
+data=urllib.urlencode(values)
+url=""
+request=urllib2.Request(url,data)   #post
+#request = urllib2.Request("%s?%s"%(url,data))   #get
+response=urllib2.urlopen(request)
+print response.read()
